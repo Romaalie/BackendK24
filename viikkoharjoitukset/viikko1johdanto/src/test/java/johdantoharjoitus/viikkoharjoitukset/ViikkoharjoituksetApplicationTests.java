8d217f1c56becba3488e69df59ae6890d2fd45af
Ali Romar <ali.romar@myy.haaga-helia.fi>
2024-01-18 10:38:37 +0200

    Hakemistorakenteen muutos viikkoharjoitukset alihakemistot

---
+++ viikkoharjoitukset/viikko1johdanto/src/test/java/johdantoharjoitus/viikkoharjoitukset/ViikkoharjoituksetApplicationTests.java
@@ -0,0 +1,13 @@
+package johdantoharjoitus.viikkoharjoitukset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViikkoharjoituksetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
