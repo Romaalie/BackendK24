6cc2d979643de9668398a33843e80a27a36131c0
Ali Romar <ali.romar@myy.haaga-helia.fi>
2024-01-27 15:10:01 +0200

    Tehtävä 3. Friend list eteenpäin.
    Friendlist itsessään toimii.
    Lisätty Friend luokka ja sen getssetsstrings.
    Lisätty friendlist.html template ja laitettu se toimimaan.
    Lisätty addFriend.html template, mutta toiminnallisuus vielä kesken.
    Poistettu People luokka.
    
    Joku ihme jämä .LCKFriend.java~ jäi kun poistelin ja lisäilin luokkia.

--- viikkoharjoitukset/viikko1johdanto/src/main/java/modelandview/web/ModelandviewController.java
+++ viikkoharjoitukset/viikko1johdanto/src/main/java/modelandview/web/ModelandviewController.java
@@ -6,9 +6,11 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import modelandview.domain.Friend;
 import modelandview.domain.Student;
 
 @Controller
@@ -28,15 +30,38 @@ public class ModelandviewController {
     }
 
     @GetMapping("/studentlist")
-    public String showStudentList(Model modelStudent) {
+    public String showStudentlist(Model modelStudent) {
         List<Student> students = new ArrayList<>();
         students.add(new Student("Ali", "Romar"));
         students.add(new Student("Markku", "Mäkipelto"));
-        students.add(new Student("Jaakko", "Seppälä"));        
+        students.add(new Student("Jaakko", "Seppälä"));
 
         modelStudent.addAttribute("students", students);
 
         return "studentlist";
     }
 
+    //@RequestMapping
+    @GetMapping("/friendlist")
+    public String showFriendList(Model modelFriend) {
+        List<Friend> friends = new ArrayList<>();
+        friends.add(new Friend("Marco", "Polo"));
+        friends.add(new Friend("George", "Harrison"));
+        friends.add(new Friend("Aleksis", "Kivi"));
+
+        modelFriend.addAttribute("friends", friends);
+
+        return "friendlist";
+    }
+
+    @GetMapping("/addFriend")
+    public String showAddFriend (Model modelFriend) {
+        return "addFriend";
+    }
+
+    @PostMapping("/addFriend")
+    public String postAddFriend(Model modelFriend) {
+
+        return "addFriend";
+    }
 }
